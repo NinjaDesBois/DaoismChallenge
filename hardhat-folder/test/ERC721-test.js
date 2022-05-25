ead590ccfe6d1f5280d3026f571c6df37e2a2fae
Ninja Coder <77759462+NinjaDesBois@users.noreply.github.com>
2022-05-26 00:14:38 +0100

    Create ERC721-test.js

--- hardhat-folder/test/ERC721-test.js
+++ hardhat-folder/test/ERC721-test.js
@@ -6,7 +6,7 @@ describe("DaoismChallengeNFT contract", function() {
     let token721;
     let _name = 'DaoismChallenge';
     let _symbol = 'DC';
-    let _baseTokenURI = 'defefedcew'
+    let _baseTokenURI = 'https://gateway.pinata.cloud/ipfs/QmXgjoa6SWs1nj6gGtpcvAD1ev24gNT6UighgyWYvgEsaV'
     let account1, otheraccounts;
     let tokenId = '0';
 
@@ -20,30 +20,47 @@ describe("DaoismChallengeNFT contract", function() {
 
     describe("Deployment", function() {
 
-        it("Should has the correct name and symbol ", async function() {
+        it("Should has the correct name ,symbol , total supply and maxtokenIds ", async function() {
             expect(await token721.name()).to.equal(_name);
             expect(await token721.symbol()).to.equal(_symbol);
+            expect(await token721.totalSupply()).to.equal('0');
+            expect(await token721.maxTokenIds()).to.equal('20');
+        });
+        // it("Should return the TokenURI", async function() {
+        //     expect(await token721._baseURI()).to.equal(_baseTokenURI);
+        // });
+        it('correctly mints a NFT', async function() {
+            expect(await token721.connect(account1).mint({
+                value: ethers.utils.parseEther("0.1")
+            })).to.emit(token721, 'Transfer');
+            expect(await token721.balanceOf(account1.address)).to.equal(1);
         });
 
-        it("Should mint a token with token ID 1 & 2 to account1", async function() {
+        it("returns correct balanceOf with token ID 1 & 2 to account1", async function() {
             const address1 = owner.address;
-            // const instance = await MyContract.at(contractAddress);
-
             await token721.mint({
-
                 value: ethers.utils.parseEther("0.1")
             });
-
             expect(await token721.ownerOf(1)).to.equal(address1);
 
             await token721.mint({
-
                 value: ethers.utils.parseEther("0.1")
             });
-
             expect(await token721.ownerOf(2)).to.equal(address1);
-
             expect(await token721.balanceOf(address1)).to.equal(2);
+
         });
+
+        it("Correctly returns the right tokenURI", async function() {
+            expect(await token721.connect(account1).mint({
+                value: ethers.utils.parseEther("0.1")
+            })).to.emit(token721, 'Transfer');
+            expect(await token721.tokenURI('1')).to.equal(`${_baseTokenURI}1`);
+        })
+
+
+
     });
+
+
 });
